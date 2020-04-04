3e3bf981b69e3fd991308768eb14e872bd440220
TukiyoIgarashi <tukiyo320@gmail.com>
2020-04-05 05:56:24 +0900

    share to twitter on saving completed

--- src/main.js
+++ src/main.js
@@ -2,14 +2,22 @@ import Vue from 'vue'
 import App from './App.vue'
 import DragDrop from 'vue-drag-drop'
 import BootstrapVue from 'bootstrap-vue'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+library.add(fab);
+
+Vue.component('v-fa', FontAwesomeIcon);
 
 Vue.use(DragDrop);
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue);
 
 new Vue({
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
